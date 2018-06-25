c3bf00130e1c5721af9e58ddb4dc3187d2b267e5
xkel <xavierk98@yahoo.com>
2018-06-25 18:34:59 -0400

    add get message request, iterate through all messages in a chat

--- index.py
+++ index.py
@@ -1,16 +1,16 @@
 import requests
 import config
+import bot
 
 def main():
-    url = 'https://api.groupme.com/v3/bots/post'
-    headers = {'Content-Type': 'application/JSON'}
-    params = {'token': config.token, 'text': 'Are my keys safe?', 'bot_id': config.bot_id}
-    makeRequest(url, headers, params)
-
-
-def makeRequest(url, headers, params):
-    r = requests.post(url, headers = headers, params = params)
-    print(r.text)
+    bot.getMessages()
 
 
 main()
+
+
+
+    #baseUrl = 'https://api.groupme.com/v3'
+    #url = '/bots/post'
+    #headers = {'Content-Type': 'application/JSON'}
+    #params = {'token': config.token, 'text': 'Can you append a url to a base url?', 'bot_id': config.bot_id}
