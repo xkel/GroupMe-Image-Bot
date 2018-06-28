b431f22ff57d1fa30085de93e0755247607acf02
xkel <xavierk98@yahoo.com>
2018-06-28 10:20:04 -0400

    create image from binary data

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@ import config
 import requests
 import json
 
+from io import BytesIO
+from PIL import Image
+
 # Group ID: 41498316
 
 def makePostRequest(url, headers = None, params = None):
@@ -26,16 +29,38 @@ def makeGetRequest(url, params, json):
     print(r)
 
 def post_data():
-    base_url = 'http://386211c5.ngrok.io'
+    base_url = 'http://c7dadf45.ngrok.io'
     payload = json.dumps({'user': 'pass'})
     
     r = requests.post(base_url, data=payload)
     print(r.status_code)
-    
+
 def groupMessage(text):
     url = '/bots/post'
     params = {'bot_id': config.bot_id, 'text': text}
     makePostRequest(url, params)
+
+def getFile(url):
+    r_file = requests.get(url)
+    content = r_file.content
+
+    img = Image.open(BytesIO(content))
+
+    #if succesful display image
+    img.show()
+
+    with open('test.txt', 'wb') as fd:
+        #for chunk in r_file.iter_content(chunk_size=128):
+            fd.write(content)
+    
+    
+
+
+    #with open(path, 'wb') as f:)
+    #    for chunk in r_file.iter_content():
+    #        f.write(chunk)
+    #local_file = open(path, 'rb')
+
 
 # Prints all messages posted in a specified group chat (group id)
 # Note: Hard-coded group ID in
@@ -51,11 +76,13 @@ def printAllMessages():
 
     while i < msg_count:
         if(x < 20):
-            print(messagesResponse['response']['messages'][x]['text'], (i+1))
+            #print(messagesResponse['response']['messages'][x]['text'], (i+1))
             if messagesResponse['response']['messages'][x]['attachments']:
-                print(messagesResponse['response']['messages'][x]['attachments'][0]['url'])
+                img_url = messagesResponse['response']['messages'][x]['attachments'][0]['url']
+                getFile(img_url)
+                break
             if(x == 19):
-               id = messagesResponse['response']['messages'][x]['id'] 
+                id = messagesResponse['response']['messages'][x]['id'] 
             x += 1
         else:
             params = {'token': config.token, 'before_id': id} # before_id, since_id, after_id
