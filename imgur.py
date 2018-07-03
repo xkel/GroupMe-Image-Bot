1e6ef3c9d5f8e02a3d2bf34f2c29a3f931df8d6c
xkel <xavierk98@yahoo.com>
2018-07-03 10:02:43 -0400

    base imgur functionality

--- imgur.py
+++ imgur.py
@@ -8,7 +8,7 @@ import json
 # imgur only allows you to post images directly to an album if they are already 'owned' by you
 # you must post an image directly to your account
 
-def imgur_post(img):
+def post_img(img):
 
     # if img is a url
     headers = {
@@ -18,5 +18,45 @@ def imgur_post(img):
         'image': img
     }
     r = requests.post('https://api.imgur.com/3/image', headers = headers, params = params)
-    print(r.text)
     print(r.status_code)
+
+def post_images(img_list):
+    for item in img_list:
+        try:
+            post_img(item)
+        except requests.exceptions.RequestsException as e:
+            print(e)
+            sys.exit(1) 
+
+
+# iterates through the account images and grabs ids
+def get_imgIDs():
+    
+    img_IDs = []
+    # get request
+
+    headers = {
+        'Authorization': 'Bearer ' + config.imgur_token
+    }
+
+    r = requests.get('https://api.imgur.com/3/account/me/images', headers = headers).json()
+    data = r['data']
+    for item in data:
+        img_IDs.append(item['id'])
+    return img_IDs
+
+def post_album(img_list):
+    headers = {
+        'Authorization': 'Bearer ' + config.imgur_token
+    }
+    params =  {
+        'ids[]': img_list
+    }
+    albumHash = 'k8I6ZgQ'
+    url = f'https://api.imgur.com/3/album/{albumHash}/add'
+    r = requests.post(url, headers=headers, params=params)
+    print(r.text)
+    
+
+
+
--- index.py
+++ index.py
@@ -1,5 +1,8 @@
 import bot
-
+import imgur
 
 if __name__ == "__main__":
-    bot.printAllMessages()
+    img_list = bot.get_images() # grabs images from group history in the GroupMe by URL
+    imgur.post_images(img_list) # posts images to the Imgur account with an array 
+    img_IDs = imgur.get_imgIDs()
+    imgur.post_album(img_IDs) 
