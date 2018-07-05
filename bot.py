aae4449fd963b07fc64873561bc70918f409c122
xkel <xavierk98@yahoo.com>
2018-07-05 13:32:39 -0400

    method wrapping

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-#import config
 import requests
 import json
 import imgur
@@ -10,31 +9,6 @@ class Bot:
         self.auth_token = auth
         self.group_id = group
 
-    def makePostRequest(self, url, params):
-        base_url = 'https://api.groupme.com/v3'
-        r = requests.post(base_url + url, params = params)
-        
-        print(r.status_code)
-        print(r.text)
-        # if headers is None:
-        #     r = requests.post(base_url + url, params = params)
-        # elif params is None:
-        #     r = requests.post(base_url + url, headers = headers)
-        # else:
-        #     r = requests.post(base_url + url, headers = headers, params = params)
- 
-        # print(r.status_code)
-        # print(r.text)
-
-    def makeGetRequest(self, url, params, json):
-        base_url = 'https://api.groupme.com/v3'
-        if json:
-            r = requests.get(base_url + url, params = params).json()
-        return r
- 
-        print(r)
-        
-    # Group ID: 41498316
     def post_data():
         base_url = 'http://0bb05726.ngrok.io'
         payload = json.dumps({'user': 'pass'})
@@ -42,12 +16,12 @@ class Bot:
         r = requests.post(base_url, data=payload)
         print(r.status_code)
 
-    def postMessage(self, text):
+    def postMessage(self, text): 
         url = '/bots/post'
         params = {'bot_id': self.bot_token, 'text': text}
         self.makePostRequest(url, params)
 
-    def getBinaryData(url):
+    def __getBinaryData(url):
         r_file = requests.get(url)
         content = r_file.content
         return content
@@ -61,11 +35,9 @@ class Bot:
         if(req.status_code == 200):
             print('success')
 
+    # Returns an array of all images shared in a group
 
-    # Prints all messages posted in a specified group chat (group id)
-    # Note: Hard-coded group ID in
-    # should be run upon joining for the first time
-    def get_images():
+    def get_images(self):
         base_url = 'https://api.groupme.com/v3'
         url = f'/groups/{self.group_id}/messages'
         params = {'token': self.auth_token} # before_id, since_id, after_id
@@ -89,3 +61,8 @@ class Bot:
                 x = 0
             i += 1
         return img_list
+
+    def post_all_images(self):
+            img_list = self.get_images()
+            imgur.post_images(img_list) # posts images to the Imgur account with an array 
+
